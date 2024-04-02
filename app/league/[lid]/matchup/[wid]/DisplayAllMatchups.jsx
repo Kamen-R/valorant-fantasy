ea0f362e90a9fd1be22721b840ba38c80ae2f85b
Kamen-R <Kamen-R@users.noreply.github.com>
2024-04-02 06:10:19 -0700

    version 1.02, added names to matchup page

--- app/league/[lid]/matchup/[wid]/DisplayAllMatchups.jsx
+++ app/league/[lid]/matchup/[wid]/DisplayAllMatchups.jsx
@@ -40,36 +40,39 @@ async function getGameData(supabase, wid) {
 
 function renderMatch(league_info, matchup, team_names, points, players){
 
-    var renderedOutputTeam1 = league_info.roster_count.map(item => 
-        <h4 key={'t1-' + item} className="team-1">{players[matchup[`team1_${item}`]] + ' ' + matchup[`team1_${item}`] + ' - ' + points[matchup[`team1_${item}`]]}</h4>
-        )
+  var renderedOutputTeam1 = league_info.roster_count.map(item => 
+      <h4 key={'t1-' + item} className="team-1">{players[matchup[`team1_${item}`]] + ' ' + matchup[`team1_${item}`] + ' - ' + points[matchup[`team1_${item}`]]}</h4>
+    )
     
-      var renderedOutputTeam2 = league_info.roster_count.map(item => 
-        <h4 key={'t2-' + item} className="team-2">{points[matchup[`team2_${item}`]] + ' - ' + players[matchup[`team2_${item}`]] + ' ' + matchup[`team2_${item}`]}</h4>
-        )
+    var renderedOutputTeam2 = league_info.roster_count.map(item => 
+      <h4 key={'t2-' + item} className="team-2">{points[matchup[`team2_${item}`]] + ' - ' + players[matchup[`team2_${item}`]] + ' ' + matchup[`team2_${item}`]}</h4>
+    )
       
-      var renderedOutputMiddle = league_info.roster_count.map(item => 
-        <h4 key={'middle-' + item} className="middle-info">{league_info.roster_spots[item]}</h4>
-        )
-
-    return (
-        <div id="flex-container" style={{paddingBottom: "10px"}}>
-          <div id="team-left">
-            <h2 id="team-name-left">{team_names[matchup.tid1].name}</h2>
-            {renderedOutputTeam1}
-          </div>
-          <div id="middle-matchup">
-            <h2 id="score-left">{matchup.score1}</h2>
-            <h2 style={{display: "inline", padding: "10px"}}>-</h2>
-            <h2 id="score-right">{matchup.score2}</h2>
-            {renderedOutputMiddle}
-          </div>
-          <div id="team-right">
-            <h2 id="team-name-right">{team_names[matchup.tid2].name}</h2>
-            {renderedOutputTeam2}
-          </div>
+    var renderedOutputMiddle = league_info.roster_count.map(item => 
+      <h4 key={'middle-' + item} className="middle-info">{league_info.roster_spots[item]}</h4>
+      )
+      
+  return (
+      <div id="flex-container" style={{paddingBottom: "10px"}}>
+        <div id="team-left">
+          <h2 id="team-name-left">{team_names[matchup.tid1].name}</h2>
+          <h3 id="team-name-left">{team_names[matchup.tid1].owner}</h3>
+          {renderedOutputTeam1}
         </div>
-    );
+        <div id="middle-matchup">
+          <h2 id="score-left">{matchup.score1}</h2>
+          <h2 style={{display: "inline", padding: "10px"}}>-</h2>
+          <h2 id="score-right">{matchup.score2}</h2>
+          <h3>Rosters</h3>
+          {renderedOutputMiddle}
+        </div>
+        <div id="team-right">
+          <h2 id="team-name-right">{team_names[matchup.tid2].name}</h2>
+          <h3 id="team-name-right">{team_names[matchup.tid2].owner}</h3>
+          {renderedOutputTeam2}
+        </div>
+      </div>
+  );
 }
 
 export default async function DisplayAllMatchups(league_info, lid, wid) {
